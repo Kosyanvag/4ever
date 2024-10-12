d697b033f528fb2777bb5a964c46f967f9be2774
Vagarshak Kosyan <vag.kosyan@gmail.com>
2024-10-12 15:02:06 +0400

    fixed some bugs velvet

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ export default function App() {
       {/* <Serene /> */}
       {/* <Harmony/> */}
       {/* <Radiance/> */}
-      <Velvet />
-      {/* <Project /> */}
+      {/* <Velvet /> */}
+      <Project />
       {/* <Slider /> */}
     </div>
   );
