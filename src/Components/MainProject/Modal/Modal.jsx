b20dbebd1e4745c105338a3c4b94cd757dacf531
arsenkoshatashyan <arsen.koshatashyan@mail.ru>
2024-10-19 00:34:12 +0400

    burger-menu

--- src/Components/MainProject/Modal/Modal.jsx
+++ src/Components/MainProject/Modal/Modal.jsx
@@ -1,8 +1,29 @@
 import "./Modal.scss";
+import { useEffect } from "react";
 
 export default function Modal({ cancelModal }) {
+
+  const handleClickOutside = (e) => {
+    if (e.target.classList.contains("Modal")) {
+      cancelModal();
+    }
+  };
+
+  useEffect(() => {
+    const handleEscape = (e) => {
+      if (e.key === "Escape") {
+        cancelModal();
+      }
+    };
+    
+    window.addEventListener("keydown", handleEscape);
+    return () => {
+      window.removeEventListener("keydown", handleEscape);
+    };
+  }, [cancelModal]);
+
   return (
-    <div className="Modal">
+    <div className="Modal" onClick={handleClickOutside}>
       <div className="Modal__content">
         <div>
           <img onClick={cancelModal} src="./images/Modal/x.svg" alt="cancel" />
@@ -10,7 +31,7 @@ export default function Modal({ cancelModal }) {
         <h2>Оформление заказа </h2>
         <form action="https://api.web3forms.com/submit" method="POST">
           <select name="Design" required>
-            <option value="Выберите ваш дизайн" selected disabled required>
+            <option value="" selected disabled>
               Выберите ваш дизайн
             </option>
             <option value="Serene">Serene</option>
@@ -24,20 +45,18 @@ export default function Modal({ cancelModal }) {
           </select>
           <input type="text" placeholder="Имя Фамилия" name="Name" required />
           <input
-            type="number"
+            type="tel"
             placeholder="Tелефон"
             name="PhoneNumber"
             required
           />
-
           <input
             type="hidden"
             name="access_key"
             value="0338c2db-c5cc-457f-89c7-b95472fdc431"
-          ></input>
-          <button>Заказать</button>
+          />
+          <button type="submit">Заказать</button>
         </form>
-        <div className="Modal__buttons"></div>
       </div>
     </div>
   );
