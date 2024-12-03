d0745f4085bd15adb022ffa70dae65f177d67b88
Vagarshak Kosyan <vag.kosyan@gmail.com>
2024-12-03 17:48:49 +0400

    translations

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 
+import "./i18n.js";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
